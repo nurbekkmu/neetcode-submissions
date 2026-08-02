3711eecbe5659b656b0d2c059d0748d56f1afbfd
NURBEK DILMUROD UGLI SUVONOV <nurbekkmu@gmail.com>
2026-08-02 15:41:47 +0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_hash = {}
+
+        for ch in s:
+            if ch in s_hash:
+                s_hash[ch] += 1
+            else:
+                s_hash[ch] = 1
+
+        for ch in t:
+            if ch not in s_hash:
+                return False
+            else:
+                s_hash[ch] -= 1
+
+
+        for val in s_hash.values():
+            if val != 0:
+                return False
+
+        return True                                    
+        
