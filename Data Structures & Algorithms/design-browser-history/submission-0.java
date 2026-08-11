e73bd7661855a4b247b7520e1a226f7d987b99f2
NURBEK DILMUROD UGLI SUVONOV <nurbekkmu@gmail.com>
2026-08-11 16:57:10 +0500

    Add: design-browser-history - submission-0

---
+++ Data Structures & Algorithms/design-browser-history/submission-0.java
@@ -0,0 +1,37 @@
+class BrowserHistory {
+
+    private List<String> history;
+    private int current;
+
+    public BrowserHistory(String homepage) {
+        history = new ArrayList<>();
+        history.add(homepage);
+        current = 0;
+    }
+    
+    public void visit(String url) {
+        while (history.size() > current + 1) {
+            history.remove(history.size() - 1);
+        }
+        history.add(url);
+        current++;
+    }
+    
+    public String back(int steps) {
+        current = Math.max(0, current - steps);
+        return history.get(current);
+    }
+    
+    public String forward(int steps) {
+        current = Math.min(history.size() - 1, current + steps);
+        return history.get(current);
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
