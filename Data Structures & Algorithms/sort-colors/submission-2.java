416f5fbbe7a575c5594f2bd86c80c24e9de6b174
NURBEK DILMUROD UGLI SUVONOV <nurbekkmu@gmail.com>
2026-08-15 05:51:34 +0500

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] counts = {0, 0, 0};
+        for (int num: nums) {
+            counts[num]++;
+        }
+
+        int i = 0;
+        for (int n = 0; n < counts.length; n++) {
+            for (int j = 0; j < counts[n]; j++) {
+                nums[i++] = n;
+            }
+        }
+    }
+}
